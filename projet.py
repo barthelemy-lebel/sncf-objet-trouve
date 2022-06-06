733a7ea743802934d5446fa8b3c641cb646d5146
barthelemy-lebel <61472040+barthelemy-lebel@users.noreply.github.com>
2022-06-06 20:41:01 +0200

    Add files via upload

--- projet.py
+++ projet.py
@@ -2,13 +2,13 @@ import tweepy
 import requests
 
 def replace_caractere(variable):
-    variable=variable.replace("\u00e0","à")
-    variable = variable.replace("\u00e9","é")
-    variable=variable.replace("\u00e7","ç")
-    variable = variable.replace("\u00e8","è")
-    variable = variable.replace("\u00ea","ê")
+    variable=variable.replace("\\u00e0","à")
+    variable=variable.replace("\\u00e9","é")
+    variable=variable.replace("\\u00e7","ç")
+    variable = variable.replace("\\u00e8","è")
+    variable = variable.replace("\\u00ea","ê")
     variable = variable.replace('"',"")
-
+    return variable
 
 def requete(url):
     """
@@ -24,19 +24,14 @@ def requete(url):
     elements=[]
     req=requests.get(url)
     req=req.text
-    
     req=req.split("},")
-    
-
-    
     req_content = req[1].split(",")
+
     for i in range(len(req_content)):
-        
         element=req_content[i].split(": ")
         elements.append(element)
 
     for i in range(len(elements)):
-        
         for j in range(len(elements[i])):
             if elements[i][j] == ' "gc_obo_gare_origine_r_name"' :
                 gare=elements[i][1]
@@ -47,8 +42,6 @@ def requete(url):
             elif ' "gc_obo_type_c"' in elements[i]:
                 objet=elements[i][1]
 
-    
-
     return date,gare,objet,nature
 
 
@@ -56,11 +49,11 @@ def requete(url):
 url="https://ressources.data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=&lang=fr&sort=date&facet=date&facet=gc_obo_date_heure_restitution_c&facet=gc_obo_gare_origine_r_name&facet=gc_obo_nature_c&facet=gc_obo_type_c&facet=gc_obo_nom_recordtype_sc_c"
 
 
-API_KEY = "KEY"
-API_SECRET = "SECRET KEY"
+API_KEY = "4z46hvPEx60NKZQkEsxDMPh8S"
+API_SECRET = "66LLwmNGaCu7BC26lxX4dGjxnU4IrimUjNQpkboKZh7VRJ38tC"
 
-ACCESS_TOKEN = "TOKEN"
-ACCESS_TOKEN_SECRET = "SECRET TOKEN"
+ACCESS_TOKEN = "1486752003179126787-o5ZBWTywbSwND7ru8S6M44uvzaPQrW"
+ACCESS_TOKEN_SECRET = "tNKsbhNeTcR5gyNgENWaaTgAnjf8kc9VOR9wzNiKUphob"
 
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -75,13 +68,13 @@ objet=requete_sncf[2]
 nature=requete_sncf[3]
 
 #réencoder les caractères
-replace_caractere(gare)
-replace_caractere(date)
-replace_caractere(objet)
-replace_caractere(nature)
+gare=replace_caractere(gare)
+date=replace_caractere(date)
+objet=replace_caractere(objet)
+nature=replace_caractere(nature)
 
-tweet="""[IMPORTANT]
-Nous avons retrouvé {0} de type {1} à la gare de {2} le {3}.""".format(objet,nature,gare,date)
+tweet="""[⚠️NOUVEL OBJET TROUVÉ⚠️]
+Les agents @SNCF ont retrouvé {0} de type {1} à la gare de {2} le {3}.""".format(objet,nature,gare,date)
 
 api.update_status(tweet)
 
